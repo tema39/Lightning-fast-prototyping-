49f7f404d06205868d7f9e1adf7750ff4cf0cdd5
unknown <arterm.nomad@mail.ru>
2020-11-19 20:54:41 +0200

    js кнопка готова

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
+const mobileMenu = document.querySelector('.header__mobile-list');
+const mobileButton = document.querySelector('.header__button-menu');
 
+mobileButton.addEventListener('click', function() {
+    if (mobileMenu.classList.contains('active')) {
+        mobileMenu.classList.remove('active');
+    } else {
+        mobileMenu.classList.add('active');
+    }
+})
 });
